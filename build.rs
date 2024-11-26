7373972d228017c54b46229ca16cb4fd728417c7
Andrew Hjortland <andrew.hjortland@icloud.com>
2024-11-25 18:07:48 -0600

    Make build script more platform independent.

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, path::PathBuf, process::Command};
+use std::{env, path::PathBuf, process::Command};
 
 use cmake::Config;
 use git2::Repository;
@@ -22,42 +22,22 @@ fn main() {
     }
 
     // Path the the Fortran source files.
-    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let fortran_dir = manifest_dir.join("REFPROP_FORTRAN");
-
-    // Define the destination directory within the cloned REFPROP-cmake repo.
-    let destination_dir = refprop_cmake_dir.join("fortran");
-
-    // Create the destination directory if it doesn't exist.
-    fs::create_dir_all(&destination_dir).expect("Failed to create destination directory.");
-
-    // Copy each Fortran file to the REFPROP-cmake repository.
-    for entry in fs::read_dir(&fortran_dir).expect("Failed to read REFPROP_FORTRAN directory.") {
-        let entry = entry.expect("Failed to get directory entry");
-        let file_path = entry.path();
-        let file_name = file_path.file_name().expect("Failed to get file name.");
-        let dest_file = destination_dir.join(file_name);
-        fs::copy(&file_path, &dest_file)
-            .unwrap_or_else(|_| panic!("Failed to copy {:?} to {:?}", file_path, dest_file));
-        println!("Copied {:?} to {:?}", file_path, dest_file);
-    }
+    let fortran_dir = PathBuf::from(
+        env::var("REFPROP_SRC_DIR").expect("REFPROP_SRC_DIR environment variable not set."),
+    );
 
     // ======================
     // Compiler and Build Configuration
     // ======================
-
     #[cfg(target_os = "macos")]
     let fortran_compiler = env::var("CMAKE_FORTRAN_COMPILER")
         .unwrap_or_else(|_| "/opt/homebrew/bin/gfortran".to_string());
 
-    let build_type = env::var("CMAKE_BUILD_TYPE").unwrap_or_else(|_| "Release".to_string());
-    // .unwrap_or_else(|_| "Debug".to_string());
     let python_executable = env::var("PYTHON_EXECUTABLE")
         .unwrap_or_else(|_| "/Users/andrew/.pyenv/versions/3.12.5/bin/python".to_string());
 
     // Optional: Check for the presence of the Fortran compiler.
     let compiler_check = Command::new(&fortran_compiler).arg("--version").output();
-
     if compiler_check.is_err() || !compiler_check.unwrap().status.success() {
         panic!(
             "Fortran compiler not found at {:?}. Please install gfortran or update the path.",
@@ -65,16 +45,23 @@ fn main() {
         );
     }
 
+    let python_check = Command::new(&python_executable).arg("--version").output();
+    if python_check.is_err() || !python_check.unwrap().status.success() {
+        panic!(
+            "Python executable not found at {:?}. Please install python or update the path.",
+            python_executable
+        );
+    }
+
+    let build_type = env::var("CMAKE_BUILD_TYPE").unwrap_or_else(|_| "Release".to_string());
+
     // Configure CMake with custom options.
     let mut cmake_config = Config::new(&refprop_cmake_dir);
     cmake_config
+        .define("REFPROP_FORTRAN_PATH", &fortran_dir)
         .define("CMAKE_FORTRAN_COMPILER", &fortran_compiler)
         .define("CMAKE_BUILD_TYPE", &build_type)
         .define("PYTHON_EXECUTABLE", &python_executable);
-    // .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY", &out_dir)
-    // .define("CMAKE_LIBRARY_ARCHIVE_DIRECTORY", &out_dir)
-    // .define("CMAKE_LIBRARY_RUNTIME_DIRECTORY", &out_dir)
-    // .define("CMAKE_SKIP_INSTALL_ALL_DEPENDENCY", "TRUE");
 
     println!("Building REFPROP-cmake library with CMake...");
 
